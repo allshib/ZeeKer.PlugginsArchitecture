6979d398daec11e228c2e723cdb7649ed3efd136
Alex Shibalkin <al.shibalkin@gmail.com>
2025-09-20 11:53:10 +0500

    Исправление ошибки

--- ZeeKer.PlugginsArchitecture.API/Program.cs
+++ ZeeKer.PlugginsArchitecture.API/Program.cs
@@ -4,22 +4,19 @@ using ZeeKer.PlugginsArchitecture.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Âåðñèÿ êîíòðàêòà (ìåíÿéòå ïðè breaking changes â Abstractions)
 var hostContractVersion = new Version(1, 0, 0);
 
-// Áàçîâûå ñåðâèñû ïðèëîæåíèÿ...
 builder.Services.AddAuthorization();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Ãðóçèì ïëàãèíû (services stage)
 var plugins = PluginLoader.LoadPlugins(
     builder.Services,
     builder.Configuration,
     Path.Combine(AppContext.BaseDirectory, "plugins"),
     hostContractVersion);
 
-PluginMapper.MapApiPlugins(app, plugins);
+
 //foreach (var p in plugins.Select(p => p.Instance))
 //{
 //    if (p is IAuthorizationContributor auth)
@@ -34,16 +31,13 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-// Ìàïèì îñíîâíûå ýíäïîéíòû ÿäðà...
 app.MapGet("/", () => Results.Ok("Host OK"));
 
-// Ìàïèì ýíäïîéíòû ïëàãèíîâ
-PlugginMapper.MapApiPlugins(app, plugins);
+PluginMapper.MapApiPlugins(app, plugins);
 
 //// Healthchecks
 //app.MapHealthChecks("/health");
 
-// Àâòîðèçàöèÿ/àóòåíòèôèêàöèÿ ïî ìåñòó
 app.UseAuthorization();
 
 app.Run();
